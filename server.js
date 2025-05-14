792dffa66b1191cd870503776b11525d9b0a8d24
Luciano-A-Vilete <luciano.a.vilete@hotmail.com>
2025-05-14 14:34:52 -0300

    Week01 activity website fixing

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
-// Index Route
+
+// Index route
 app.get("/", function(req, res){
   res.render("index", {title: "Home"})
 })
